46ec01032f762f777786815ee6b2e682b8ac6d5b
joshgaber <joshgaber@gmail.com>
2020-01-11 15:24:53 -0500

    Show env vars in component

--- client/src/views/Jellybean/Dashboard.vue
+++ client/src/views/Jellybean/Dashboard.vue
@@ -2,5 +2,20 @@
   <div class="h-screen w-screen flex flex-col justify-around">
     <h1>Jellybean Contest</h1>
     <div class="flex"></div>
+    <div>
+      Wanna play? Go to <span>{{ host }}</span>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      host: process.env.VUE_APP_HOST + '/jellybean',
+      jellybeans: parseInt(process.env.VUE_APP_JELLYBEANS),
+      guesses: [],
+    };
+  },
+};
+</script>
