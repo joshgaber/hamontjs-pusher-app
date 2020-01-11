582145a26a4b808cdf37a2426f7957bae390f89b
joshgaber <joshgaber@gmail.com>
2020-01-11 15:02:34 -0500

    Add pusher and configure jellybean endpoints

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
-const express = require('express')
+const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static('static'));
+const Pusher = require('pusher');
+const pusher = new Pusher({
+  appId: '909292',
+  key: '3d63639268dbf6809285',
+  secret: 'fa823a3af22294eaf262',
+  cluster: 'us2',
+  useTLS: true,
+});
 
 app.post('/api/guess', function(req, res) {
-
+  const { name, guess } = req.body;
+  pusher.trigger('hamontjs', 'jellybean-guess', { name, guess });
+  res.send();
 });
 
 app.post('/api/results', function(req, res) {
-
+  const { name, guess } = req.body;
+  pusher.trigger('hamontjs', 'jellybean-winner', { name, guess });
+  res.send();
 });
 
 app.get('/', (req, res) => {
-    res.send('Test');
+  res.send({
+    host: req.hostname,
+    port: port,
+  });
 });
 
 app.listen(port, () => console.log(`App is running on port ${port}!`));
