fd7caed453d7cd29b18ad2eb50b2f16e7828c770
joshgaber <joshgaber@gmail.com>
2020-02-01 00:40:58 -0500

    Add chatroom routes with form-data parser

--- index.js
+++ index.js
@@ -5,13 +5,16 @@ const bodyParser = require('body-parser');
 
 const jellybean = require('./apps/jellybean');
 const tictactoe = require('./apps/tictactoe');
+const chatroom = require('./apps/chatroom');
 
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(jellybean);
 app.use(tictactoe);
+app.use(chatroom);
 
 app.get('/', (req, res) => {
   res.send({
